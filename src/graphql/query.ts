c86da891e2449842d4d0250030f34202d7f83398
marufa75 <r.meinardi@gmail.com>
2022-03-06 19:50:58 +0100

    query project

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -18,5 +18,14 @@ export const Query = objectType({
         })
       },
     })
+    t.field('project', {
+      type: 'Project',
+      args: { id: nonNull(idArg()) , organizationId: nonNull(idArg()) },
+      resolve: (_parent, args, context) => {
+        return context.prisma.project.findFirst({
+          where: { id: args.id, organizationId: args.organizationId },
+        })
+      },
+    })
   },
 })
